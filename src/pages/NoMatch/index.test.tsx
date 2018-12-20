7c459104d4f2bd91ff9576b6a68e195bc13f7b92
TinyKitten <catwalk8205@gmail.com>
2018-12-20 10:39:22 +0900

    enzyme採用

--- src/pages/NoMatch/index.test.tsx
+++ src/pages/NoMatch/index.test.tsx
@@ -1,9 +1,8 @@
+import { shallow } from "enzyme";
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 import { NoMatch } from ".";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<NoMatch />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = shallow(<NoMatch />);
+  expect(wrapper.find("h1").text()).toBeDefined();
 });
