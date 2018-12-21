405bd266a9771fb4f7499b8eaf04cad2bae93dca
TinyKitten <catwalk8205@gmail.com>
2018-12-22 00:03:00 +0900

    PMSS用

--- src/pages/QR/index.tsx
+++ src/pages/QR/index.tsx
@@ -96,7 +96,14 @@ const YourQR = (props: IProps) => (
     <YourQRHeading>
       {props.token ? JSON.parse(props.token).name : null}さんの参加証
     </YourQRHeading>
-    <QRCode size={250} value={props.token ? base64Encode(props.token) : ""} />
+    <QRCode
+      size={250}
+      value={
+        props.token
+          ? `https://pmss.teamkitten.tk/${base64Encode(props.token)}`
+          : ""
+      }
+    />
   </YourQRWrapper>
 );
 
